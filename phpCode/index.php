cf011cab2f6888a7499cdd072f05655a2477b78e
tom38448 <jesperapps.integration@gmail.com>
2024-08-28 16:48:14 +0530

    Add files via upload

---
+++ phpCode/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Demo PHP Page</title>
+    <style>
+        body {
+            background-color: green;
+            color: white; /* Adding white text color for better readability */
+        }
+    </style>
+</head>
+<body>
+    <?php
+    echo "Hello, world!\n";
+    echo "version 3";
+    ?>
+</body>
+</html>
